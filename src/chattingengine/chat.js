c2d3912974a906af102fe6b9827e7487ae235e1b
tim-hub <tim_w@outlook.com>
2019-06-13 18:05:21 +1200

    add an async chatting function

---
+++ src/chattingengine/chat.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const data = require(path.join(__dirname, "./data.json"));
+
+const answer = question => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      try {
+        let reply =
+          data.greetings[Math.floor(Math.random() * data.greetings.length) + 1];
+        console.log("resolve" + question);
+        resolve(reply["content"]);
+      } catch (e) {
+        console.log("reject" + question);
+        reject(e);
+      }
+    }, Math.floor(Math.random() * 100) + 1);
+  });
+};
+
+async function asynAnswer(question) {
+  const result = await answer(question).catch(e => {
+    console.log(e);
+  });
+  console.log(result);
+  return result;
+}
+
+asynAnswer("123");
+
+// export default asynAnswer;

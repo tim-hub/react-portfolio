e110d7bd8dc5031f34e23b8a7ca1a11037ecd6e7
tim-hub <tim_w@outlook.com>
2019-06-20 10:43:27 +1200

    Updates

--- precache-manifest.f5df51eecb447ada02731cecfa54e97e.js
+++ precache-manifest.f5df51eecb447ada02731cecfa54e97e.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6c1cb916a5b01d4c5a5ba74861056409",
-    "url": "/react-portfolio/index.html"
+    revision: "6c1cb916a5b01d4c5a5ba74861056409",
+    url: "/react-portfolio/index.html"
   },
   {
-    "revision": "594004e477b8ef3656be",
-    "url": "/react-portfolio/static/css/main.8e3ecc79.chunk.css"
+    revision: "594004e477b8ef3656be",
+    url: "/react-portfolio/static/css/main.8e3ecc79.chunk.css"
   },
   {
-    "revision": "adf67b29ab0c61347a33",
-    "url": "/react-portfolio/static/js/2.c22a513a.chunk.js"
+    revision: "adf67b29ab0c61347a33",
+    url: "/react-portfolio/static/js/2.c22a513a.chunk.js"
   },
   {
-    "revision": "594004e477b8ef3656be",
-    "url": "/react-portfolio/static/js/main.12e8b8db.chunk.js"
+    revision: "594004e477b8ef3656be",
+    url: "/react-portfolio/static/js/main.12e8b8db.chunk.js"
   },
   {
-    "revision": "94872cf7e81768d148a3",
-    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+    revision: "94872cf7e81768d148a3",
+    url: "/react-portfolio/static/js/runtime~main.f974a8eb.js"
   }
 ]);

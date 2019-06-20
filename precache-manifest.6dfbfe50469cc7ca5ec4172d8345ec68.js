bf683f7569518b0c161e3250421a1d0710783368
tim-hub <tim_w@outlook.com>
2019-06-20 22:38:09 +1200

    Updates

---
+++ precache-manifest.6dfbfe50469cc7ca5ec4172d8345ec68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "685778a7d99ef7e01b7a7b4ed212fbe4",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "5c4d9280258de453a905",
+    "url": "/react-portfolio/static/css/main.5f6b0ecd.chunk.css"
+  },
+  {
+    "revision": "77df086d3483bb346d69",
+    "url": "/react-portfolio/static/js/2.d17f658f.chunk.js"
+  },
+  {
+    "revision": "5c4d9280258de453a905",
+    "url": "/react-portfolio/static/js/main.4f3ebea7.chunk.js"
+  },
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.e6786ee1e75cc6bc5c860a61ae80ca9c.js"
+  "/react-portfolio/precache-manifest.6dfbfe50469cc7ca5ec4172d8345ec68.js"
 );
 
 self.addEventListener('message', (event) => {

bed9b0c330da8f65a1755f0ba01c3b927bb5689b
tim-hub <tim_w@outlook.com>
2019-06-21 10:31:11 +1200

    add a default 404 page

--- src/components/utils/Routers.jsx
+++ src/components/utils/Routers.jsx
@@ -1,15 +1,34 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 // import AppBarContainer from "./components/layout/AppBar";
 // import SideBar from "./components/layout/SideBar";
 import Home from "../Home";
 import Ai from "../Ai";
+import { Box, Heading, Markdown } from "grommet";
 
 const Routers = () => (
   <Switch>
     {/* <Redirect exact path="/" to="/" /> */}
-    <Route path="/home" component={Home} />
+    <Route exact path="/" component={Home} />
+    <Redirect path="/home" to="/" />
     <Route path="/ai" component={Ai} />
+    <Route
+      render={() => (
+        <Box
+          flex
+          align="center"
+          justify="center"
+          gap={"small"}
+          direction={"column"}
+        >
+          <Heading> 404 </Heading>
+          <Markdown>Cannot be found.</Markdown>
+          <Markdown>
+            You can go to the home page to know more [Go Back](/)
+          </Markdown>
+        </Box>
+      )}
+    />
   </Switch>
 );
 
